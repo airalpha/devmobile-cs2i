24bfd4b9dc57b5b3b23915efcd3f62e7c3bb3acd
AIRALPHA <allphaair@gmail.com>
2021-04-05 15:05:04 +0200

    :camera_flash: Create camera app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Camera } from '@ionic-native/camera/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [Camera, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { Camera } from '@ionic-native/camera/ngx';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  imgUrl = "assets/empty.jpg";
+
+  constructor(private camera: Camera) { }
+
+  getCamera() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then(response => {
+      this.imgUrl = 'data:image/jpeg;base64,' + response;
+    }).catch(e => {
+      console.log(e);
+    })
+  }
+
+  getGallery() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then(response => {
+      this.imgUrl = 'data:image/jpeg;base64,' + response;
+    }).catch(e => {
+      console.log(e);
+    })
+  }
 
 }
